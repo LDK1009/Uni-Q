ae376f53c887a4bebda3cd42b270519aa90cb758
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-03-08 12:03:50 +0900

    ✨ Feat : 유저 로그인 여부 검사 기능 개발

--- project/src/service/auth.ts
+++ project/src/service/auth.ts
@@ -16,18 +16,23 @@ export async function signUp({ email, password }: SignUpParamType) {
 }
 
 ////////// 로그인
-type SignInParamType = {
-  email: string;
-  password: string;
-};
-
-export async function signIn({ email, password }: SignInParamType) {
-  const response = await supabase.auth.signInWithPassword({
-    email,
-    password,
+export async function signIn() {
+  const response = await supabase.auth.signInWithOAuth({
+    provider: "kakao",
   });
 
   return response;
+}
+
+////////// 유저 로그인 여부 확인하기
+export async function getCurrentUserIsSignIn() {
+  const {error} = await supabase.auth.getUser();
+
+  if (!error) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 ////////// 현재 로그인한 유저 정보 가져오기
@@ -36,6 +41,18 @@ export async function getCurrentUserUID() {
 
   const response = {
     data: data.user?.id,
+    error,
+  };
+
+  return response;
+}
+
+////////// 현재 로그인한 유저 이메일 가져오기
+export async function getCurrentUserEmail() {
+  const { data, error } = await supabase.auth.getUser();
+
+  const response = {
+    data: data.user?.email,
     error,
   };
 
